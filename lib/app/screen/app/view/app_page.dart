e3cb30755d2c8ff807ab93b524d86ce42bc14800
flesermihai <mihai.fleser@x2mobile.net>
2024-05-10 15:22:24 +0300

    [X2-232] Merge conflicts.

--- lib/app/screen/app/view/app_page.dart
+++ lib/app/screen/app/view/app_page.dart
@@ -64,11 +64,7 @@ class _AppState extends State<AppView> {
           useMaterial3: true,
         ),
         home: BlocBuilder<AppBloc, AppState>(builder: (_, state) {
-          if (state.user == null) {
-            return const LoginPage();
-          } else {
-            return HomePage(state.user!.displayName);
-          }
+          return state.user == null ? const LoginPage() : HomePage(state.user!.displayName);
         }));
   }
 }
